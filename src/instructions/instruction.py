af3888eb22a4906e6432d7a3f14ddd219b210b6f
mingz <305603665@qq.com>
2020-02-11 02:10:45 +0800

    lang

--- src/instructions/instruction.py
+++ src/instructions/instruction.py
@@ -20,147 +20,148 @@ idx 8-15 8
 
 """
 import functools
+import struct
 
 
 class Executes(object):
     def __init__(self):
         pass
 
-    def nop(self, opcode, vm):
+    def nop(self, inst, vm):
         """"""
 
-    def add(self, opcode, vm):
+    def add(self, inst, vm):
         vm.cur_frame.add()
 
-    def sub(self, opcode, vm):
+    def sub(self, inst, vm):
         vm.cur_frame.sub()
 
-    def mul(self, opcode, vm):
+    def mul(self, inst, vm):
         vm.cur_frame.mul()
 
-    def div(self, opcode, vm):
+    def div(self, inst, vm):
         vm.cur_frame.div()
 
-    def rem(self, opcode, vm):
+    def rem(self, inst, vm):
         vm.cur_frame.rem()
 
-    def call(self, opcode, vm):
-        nArgs, nResults = opcode << 0xff, opcode << 0xffff
+    def call(self, inst, vm):
+        nArgs, nResults = inst << 0xff, inst << 0xffff
         vm.call(nArgs, nResults)
 
-    def ret(self, opcode, vm):
+    def ret(self, inst, vm):
         vm.ret()
 
-    def lc(self, opcode, vm):
-        vm.cur_frame.load_const(opcode.idx)
+    def lc(self, inst, vm):
+        vm.cur_frame.load_const(inst.idx)
 
-    def sc(self, opcode, vm):
-        vm.cur_frame.store_const(opcode.idx)
+    def sc(self, inst, vm):
+        vm.cur_frame.store_const(inst.idx)
 
-    def ln(self, opcode, vm):
-        vm.cur_frame.load_name(opcode.idx)
+    def ln(self, inst, vm):
+        vm.cur_frame.load_name(inst.idx)
 
-    def sn(self, opcode, vm):
-        vm.cur_frame.store_name(opcode.idx)
+    def sn(self, inst, vm):
+        vm.cur_frame.store_name(inst.idx)
 
-    def j(self, opcode, vm):
-        vm.add_pc(opcode.idx)
+    def j(self, inst, vm):
+        vm.add_pc(inst.idx)
 
-    def ml(self, opcode, vm):
+    def ml(self, inst, vm):
         """
         make list
         """
 
-    def mf(self, opcode, vm):
+    def mf(self, inst, vm):
         """make function"""
 
-    def push(self, opcode, vm):
+    def push(self, inst, vm):
         """"""
 
-    def pop(self, opcode, vm):
+    def pop(self, inst, vm):
         """"""
 
-    def eq(self, opcode, vm):
+    def eq(self, inst, vm):
         """"""
 
-    def neq(self, opcode, vm):
+    def neq(self, inst, vm):
         """"""
 
-    def lt(self, opcode, vm):
+    def lt(self, inst, vm):
         """"""
 
-    def lte(self, opcode, vm):
+    def lte(self, inst, vm):
         """"""
 
-    def gt(self, opcode, vm):
+    def gt(self, inst, vm):
         """"""
 
-    def gte(self, opcode, vm):
+    def gte(self, inst, vm):
         """"""
 
-    def is_(self, opcode, vm):
+    def is_(self, inst, vm):
         """"""
 
-    def in_(self, opcode, vm):
+    def in_(self, inst, vm):
         """"""
 
-    def or_(self, opcode, vm):
+    def or_(self, inst, vm):
         """"""
 
-    def and_(self, opcode, vm):
+    def and_(self, inst, vm):
         """"""
 
-    def not_(self, opcode, vm):
+    def not_(self, inst, vm):
         """"""
 
-    def print(self, opcode, vm):
+    def print(self, inst, vm):
         """"""
 
 
 e = Executes()
 
 opcodes = [
-    ('idx', 'opcode', 'type', 'name', 'do', 'desc'),
+    ('idx 8bit ', 'opcode 7bit', 'type 1bit', 'name', 'do', 'desc'),
 
     (0b00000000, 0b0000000, 0b0, 'nop', e.nop, ''),
 
-    (0b00000000, 0b0000000, 0b0, 'add', e.add, ''),
-    (0b00000000, 0b0000000, 0b0, 'sub', e.sub, ''),
-    (0b00000000, 0b0000000, 0b0, 'mul', e.mul, ''),
-    (0b00000000, 0b0000000, 0b0, 'div', e.div, ''),
-    (0b00000000, 0b0000000, 0b0, 'rem', e.rem, ''),
+    (0b00000000, 0b0000001, 0b0, 'add', e.add, ''),
+    (0b00000000, 0b0000010, 0b0, 'sub', e.sub, ''),
+    (0b00000000, 0b0000011, 0b0, 'mul', e.mul, ''),
+    (0b00000000, 0b0000100, 0b0, 'div', e.div, ''),
+    (0b00000000, 0b0000101, 0b0, 'rem', e.rem, ''),
 
-    (0b00000000, 0b0000000, 0b0, 'call', e.call, ''),
-    (0b00000000, 0b0000000, 0b0, 'ret', e.ret, ''),
+    (0b00000000, 0b0000110, 0b0, 'call', e.call, ''),
+    (0b00000000, 0b0000111, 0b0, 'ret', e.ret, ''),
 
-    (0b00000000, 0b0000000, 0b1, 'lc', e.lc, 'load const'),
-    (0b00000000, 0b0000000, 0b1, 'sc', e.sc, 'store const'),
-    (0b00000000, 0b0000000, 0b1, 'ln', e.ln, 'load name'),
-    (0b00000000, 0b0000000, 0b1, 'sn', e.sn, 'store name'),
+    (0b00000000, 0b0001000, 0b1, 'lc', e.lc, 'load const'),
+    (0b00000000, 0b0001001, 0b1, 'sc', e.sc, 'store const'),
+    (0b00000000, 0b0001010, 0b1, 'ln', e.ln, 'load name'),
+    (0b00000000, 0b0001011, 0b1, 'sn', e.sn, 'store name'),
 
-    (0b00000000, 0b0000000, 0b1, 'j', e.j, 'jmp'),
+    (0b00000000, 0b0001100, 0b1, 'j', e.j, 'jmp'),
 
-    (0b00000000, 0b0000000, 0b1, 'ml', e.ml, 'make list'),
+    (0b00000000, 0b0001101, 0b1, 'ml', e.ml, 'make list'),
 
-    (0b00000000, 0b0000000, 0b1, 'mf', e.mf, 'make function'),
+    (0b00000000, 0b0001110, 0b1, 'mf', e.mf, 'make function'),
 
-    (0b00000000, 0b0000000, 0b1, 'push', e.push, 'push'),
-    (0b00000000, 0b0000000, 0b1, 'pop', e.pop, 'pop'),
+    (0b00000000, 0b0001111, 0b1, 'push', e.push, 'push'),
+    (0b00000000, 0b0010000, 0b1, 'pop', e.pop, 'pop'),
 
-    (0b00000000, 0b0000000, 0b1, 'eq', e.eq, 'equal'),
-    (0b00000000, 0b0000000, 0b1, 'neq', e.neq, 'not equal'),
-    (0b00000000, 0b0000000, 0b1, 'lt', e.lt, 'less than'),
-    (0b00000000, 0b0000000, 0b1, 'lte', e.lte, 'less than or equal'),
-    (0b00000000, 0b0000000, 0b1, 'gt', e.gt, 'greater than'),
-    (0b00000000, 0b0000000, 0b1, 'gte', e.gte, 'greater than or equal'),
+    (0b00000000, 0b0010001, 0b1, 'eq', e.eq, 'equal'),
+    (0b00000000, 0b0010010, 0b1, 'neq', e.neq, 'not equal'),
+    (0b00000000, 0b0010011, 0b1, 'lt', e.lt, 'less than'),
+    (0b00000000, 0b0010100, 0b1, 'lte', e.lte, 'less than or equal'),
+    (0b00000000, 0b0010101, 0b1, 'gt', e.gt, 'greater than'),
+    (0b00000000, 0b0010110, 0b1, 'gte', e.gte, 'greater than or equal'),
 
-    (0b00000000, 0b0000000, 0b1, 'is', e.is_, 'is'),
-    (0b00000000, 0b0000000, 0b1, 'in', e.in_, 'in'),
-    (0b00000000, 0b0000000, 0b1, 'or', e.or_, 'or'),
-    (0b00000000, 0b0000000, 0b1, 'and', e.and_, 'and'),
-    (0b00000000, 0b0000000, 0b1, 'not', e.not_, 'not'),
+    (0b00000000, 0b0010111, 0b1, 'is', e.is_, 'is'),
+    (0b00000000, 0b0011000, 0b1, 'in', e.in_, 'in'),
+    (0b00000000, 0b0011001, 0b1, 'or', e.or_, 'or'),
+    (0b00000000, 0b0011010, 0b1, 'and', e.and_, 'and'),
+    (0b00000000, 0b0011011, 0b1, 'not', e.not_, 'not'),
 
-    (0b00000000, 0b0000000, 0b1, 'print', e.print, 'print'),
+    (0b00000000, 0b0011100, 0b1, 'print', e.print, 'print'),
 ]
 
 opcode_map = {opcode[0]: opcode for opcode in opcodes}
@@ -170,6 +171,23 @@ opcode_map = {opcode[0]: opcode for opcode in opcodes}
 
 
 class Instruction(int):
+
+    def to_bytearray(self):
+        """"""
+        bb = struct.pack('<i', self)
+        bbb = bytearray(bb)
+        return bbb
+
+    @classmethod
+    def from_bytearray(cls, ba):
+        """"""
+        assert isinstance(ba, bytearray)
+        assert len(ba) == 2
+
+        # bb = bytearray(ba)
+        # bbb = bytearray(4 - len(bb)) + bb  # 扩展为32位
+        i, = struct.unpack('<i', ba)  # 转成int
+        return cls(i)
 
     @classmethod
     def from_inst(cls, inst):
